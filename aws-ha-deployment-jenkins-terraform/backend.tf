88736977ec0dbea1867abcc10ca0d219e27354e4
Soumyadip <96393657+Soumya14041987@users.noreply.github.com>
2024-02-25 23:39:58 +0530

    Delete aws-ha-deployment-jenkins-terraform directory

--- aws-ha-deployment-jenkins-terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "one-tier-architecture-app"
-    key    = "terraform/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- aws-ha-deployment-jenkins-terraform/output.tf
+++
@@ -1,3 +0,0 @@
-output "dns" {
-  value = aws_lb.alb.dns_name
-}
